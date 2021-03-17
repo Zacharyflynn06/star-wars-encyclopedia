9f0de7a67768a9091d4ac30e41e0d32e104fa431
Zac <zacharyflynn06@gmail.com>
2021-03-16 22:27:25 -0600

    update film index view

--- app/models/concerns/api.rb
+++ app/models/concerns/api.rb
@@ -8,6 +8,19 @@ module Api
         PEOPLE = "people/"
         FILMS =  "films/"
 
+        PAGES = ["http://swapi.dev/api/people/?page=1",
+            "http://swapi.dev/api/people/?page=2",
+            "http://swapi.dev/api/people/?page=3",
+            "http://swapi.dev/api/people/?page=4",
+            "http://swapi.dev/api/people/?page=5",
+            "http://swapi.dev/api/people/?page=6",
+            "http://swapi.dev/api/people/?page=7",
+            "http://swapi.dev/api/people/?page=8",
+            "http://swapi.dev/api/people/?page=9",]
+
+
+            
+
         def get_films
             uri = URI.parse(BASE_URL + FILMS)
             response = Net::HTTP.get_response(uri)
@@ -17,7 +30,7 @@ module Api
 
 
         def get_characters
-            uri = URI.parse(BASE_URL + PEOPLE)
+            uri = URI.parse(BASE_URL + page)
             response = Net::HTTP.get_response(uri)
             JSON.parse(response.body)
         end
